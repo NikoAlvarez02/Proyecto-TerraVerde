87d3b35a63f84227fa9dc259054c507f8f97f712
Martín Rodolfo Chazarreta <martchaz84@gmail.com>
2025-11-01 14:39:53 -0300

    migrations(usuarios): add 0007_perfil_foto ImageField for avatar

---
+++ backend/apps/usuarios/migrations/0007_perfil_foto.py
@@ -0,0 +1,16 @@
+﻿from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('usuarios', '0006_rename_usuarios_lo_username_9dce2a_idx_usuarios_lo_usernam_c124b0_idx_and_more'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='perfil',
+            name='foto',
+            field=models.ImageField(upload_to='usuarios/avatars/', null=True, blank=True),
+        ),
+    ]
